f9bb6ddb2df0f09a80b16aeb7c7e728a802426d0
Ryan Peacock <ryanxtry@gmail.com>
2026-06-02 12:45:18 -0700

    added shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-export default function App() {
+import { Button } from "@/components/ui/button";
+
+function App() {
   return (
     <div>
-      <h1 className="bold text-green-600 italic underline mb-4 text-xl text-center">
-        Yo what the frick is up Dennys?!?!
-      </h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
+
+export default App;
